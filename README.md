433eec9539aa7d272850cc964eceee07280af9d4
bwv1041 <56183117+bwv1041@users.noreply.github.com>
2019-11-03 22:24:22 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,41 +1,108 @@
-# homebridge-mi-remote
- Mi Universal Remote plugin for homebridge
- - foaked from [WestCoast5550/homebridge-mi-ir-remote](https://github.com/WestCoast5550/homebridge-mi-ir-remote)
+# [**Xiaomi IR Remote**](https://www.home-assistant.io/integrations/remote.xiaomi_miio) platform
+
+A [`homebridge-plugin`](https://www.npmjs.com/search?q=keywords:homebridge-plugin) for **Mi Universal Remote** devices
+: `chuangmi.ir.v2` `chuangmi.remote.h102a03` `chuangmi.remote.v2` `chuangmi.remote.h102c01`
+
+![米家万能遥控器](http://cdn.cnbj1.fds.api.mi-img.com/mi-mall/6c94b247060499a08307809ab8dcf5e1.jpg)
+
+> foaked from [`homebridge-mi-ir-remote`](https://www.npmjs.com/package/homebridge-mi-ir-remote/v/0.1.0) and [`homebridge-mi-ir-electrolux`](https://www.npmjs.com/package/homebridge-mi-ir-electrolux/v/0.2.2).
 
 ## Installation
- Install [`homebridge`](https://github.com/nfarina/homebridge/blob/master/README.md).
-```
-npm install -g --unsafe-perm homebridge
+
+Install [`homebridge`](https://github.com/nfarina/homebridge/blob/master/README.md).
+
+```bash
+sudo npm install -g --unsafe-perm homebridge
 ``` 
- Install [`miio`](https://github.com/aholstenson/miio/blob/master/README.md) and the plugin.
-```
-npm install -g miio homebridge-mi-remote
-```
- Get token of your Mi Universal Remote device. See detailed [instructions](https://github.com/jghaanstra/com.xiaomi-miio/blob/master/docs/obtain_token.md).
 
-## Supported Types
-1. MiLearn
-2. Bundled accessories
-* Switch
-* Light
-* Projector
-* AirConditioner
-* Custom
-* MomentarySwitch
- See detailed [instructions](https://github.com/WestCoast5550/homebridge-mi-ir-remote/blob/master/README.md) for details. 
-3. Additional supproted devicesde:
-* lg-air-conditioner: Remote control for LG Room Air Conditioner (Heater/Cooler). Compatible with AKB73835317 or AKB73675605(KR).
- Also functional on following models: LA090HSV4 LA120HSV4 LA180HSV4 LAN090HSV4 LAN120HSV4 LAN180HSV4 LS090HSV4 LS120HSV4 LS180HSV4 LSN090HSV4 LSN120HSV4 LSN180HSV4 LSU090HSV4 LSU120HSV4 LSU180HSV4 . 
+Install [`miio`](https://github.com/aholstenson/miio/blob/master/README.md) and the plugin packages.
 
-## Configuration
+```bash
+sudo npm install -g miio homebridge-mi-remote
 ```
+
+Get the [token](https://github.com/jghaanstra/com.xiaomi-miio/blob/master/docs/obtain_token.md) of your **Mi Universal Remote** device. Follow the [instruction](https://www.home-assistant.io/integrations/vacuum.xiaomi_miio/#retrieving-the-access-token).
+
+```bash
+miio --discover
+```
+
+Add the [configuration](#configuration) into the `config.json` file.
+
+## Supported types
+
+### MiLearn
+
+Learn [raw codes](https://www.home-assistant.io/integrations/remote.xiaomi_miio/#raw) from each command by IR remote controllers. To obtain the code stored in the log file (e.g., `'/var/log/homebridge.log'`), run the _bash_ script as below:
+```bash
+tail <'/var/log/homebridge.log'> | grep -oe 'Learned Code:.*' | cut -d ' ' -f3 | tail -1
+```
+
+### Bundled accessories
+
+See the [instruction](https://github.com/WestCoast5550/homebridge-mi-ir-remote/blob/master/README.md#supported-types) for details. 
+
+* **Switch**
+* **Light**
+* **Projector**
+* **AirConditioner**
+* **Custom**
+: Run multiple commands in a single switch. 
+* **MomentarySwitch**
+: Automatically turned off after 0.3 seconds. 
+
+### Additional supported devices
+
+* **LGAirConditioner**
+: IR remote control for [**LG Home Air Conditioners**](https://www.lg.com/my/home-air-conditioners) (Heater/Cooler). Compatible with [AKB73835317](https://www.google.com/search?q=AKB73835317) [AKB73675605](https://www.google.com/search?q=AKB73675605). Also functional on LA090HSV4 LA120HSV4 LA180HSV4 LAN090HSV4 LAN120HSV4 LAN180HSV4 LS090HSV4 LS120HSV4 LS180HSV4 LSN090HSV4 LSN120HSV4 LSN180HSV4 LSU090HSV4 LSU120HSV4 LSU180HSV4 . 
+
+## Configuration 
+
+Example:
+```json
 "platforms": [
     {
         "platform": "MiRemote",
         "ip": "***.***.***.***",
         "token": "********************************",
-        "hidelearn": false,
+        "hideLearn": false,
         "deviceCfgs": [
+            {
+                "type": "Switch",
+                "data": {
+                    
+                }
+            },
+            {
+                "type": "Projector",
+                "data": {
+                    
+                }
+            },
+            {
+                "type": "Light",
+                "data": {
+                    
+                }
+            },
+            {
+                "type": "AirConditioner",
+                "data": {
+                    
+                }
+            },
+            {
+                "type": "Custom",
+                "data": {
+                    
+                }
+            },
+            {
+                "type": "MomentarySwitch",
+                "data": {
+                    
+                }
+            },
             {
                 "type": "lg-air-conditioner",
                 "name": "Thermostat",
@@ -52,3 +119,8 @@ npm install -g miio homebridge-mi-remote
     }
 ]
 ```
+
+## Related websites
+###
+
+* [`python-miio`](https://github.com/rytilahti/python-miio): Python library & console tool for controlling Xiaomi smart appliances
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("./devices/ir-remote-light");
 require("./devices/ir-remote-projector");
 require("./devices/ir-remote-momentary-switch");
 require("./devices/ir-remote-air-conditioner");
-require("./devices/lg-whisen-air-conditioner");
+require("./devices/lg-air-conditioner");
 
 const miio = require('miio');
 var homebridgeAPI;
@@ -30,7 +30,16 @@ function MiRemotePlatform(log, config, api) {
     this.HomebridgeAPI = homebridgeAPI;
 
     this.log = log;
+
     this.config = config;
+    if (!this.config.ip) {
+        if (!this.config.host) throw new Error('You must provide host IP address of the device.');
+        else this.config.ip = this.config.host;
+    }
+    if (!this.config.token) {
+        throw new Error('You must provide token of the device.');
+    }
+
     this.api = api;
     this.api.on('didFinishLaunching', () => {
         //this.log.info("Done!");
@@ -42,40 +51,62 @@ function MiRemotePlatform(log, config, api) {
 
 MiRemotePlatform.prototype.accessories = function (callback) {
     var LoadedAccessories = [];
-    if (this.config['hidelearn'] == false) {
-        LoadedAccessories.push(new MiRemoteLearn(this, this.config['learnconfig']));
+    if (!this.config.hideLearn && !this.config.hidelearn) {
+        LoadedAccessories.push(new MiRemoteLearn(this, {
+                ip: this.config.ip,
+                token: this.config.token
+            }));
     }
-    var deviceCfgs = this.config['deviceCfgs'];
+    var deviceCfgs = this.config.deviceCfgs;
 
     if (deviceCfgs instanceof Array) {
         for (var i = 0; i < deviceCfgs.length; i++) {
             var deviceCfg = deviceCfgs[i];
             if (deviceCfg.type == null || deviceCfg.type == "") continue;
+            if (deviceCfg.ip == null || deviceCfg.ip == "") deviceCfg.ip = this.config.ip;
+            if (deviceCfg.token == null || deviceCfg.token == "") deviceCfg.token = this.config.token;
 
             switch (deviceCfg.type) {
                 case "Switch":
+                    if (deviceCfg.Name == null || deviceCfg.Name == "") deviceCfg.Name = deviceCfg.name || deviceCfg.type;
+                    if (deviceCfg.data == null || deviceCfg.data == "") continue;
                     LoadedAccessories.push(new MiRemoteSwitch(this, deviceCfg));
                     break;
                 case "Light":
+                    if (deviceCfg.Name == null || deviceCfg.Name == "") deviceCfg.Name = deviceCfg.name || deviceCfg.type;
+                    if (deviceCfg.data == null || deviceCfg.data == "") continue;
                     LoadedAccessories.push(new MiRemoteLight(this, deviceCfg));
                     break;
                 case "Projector":
+                    if (deviceCfg.Name == null || deviceCfg.Name == "") deviceCfg.Name = deviceCfg.name || deviceCfg.type;
+                    if (deviceCfg.data == null || deviceCfg.data == "") continue;
+                    if (deviceCfg.data.interval != null) deviceCfg.interval = deviceCfg.data.interval;
+                    if (deviceCfg.interval == null || deviceCfg.interval == "") continue;
                     LoadedAccessories.push(new MiRemoteProjector(this, deviceCfg));
                     break;
                 case "AirConditioner":
+                    if (deviceCfg.Name == null || deviceCfg.Name == "") deviceCfg.Name = deviceCfg.name || deviceCfg.type;
+                    if (deviceCfg.data == null || deviceCfg.data == "") continue;
+                    if (deviceCfg.DefaultTemperature == null) if (deviceCfg.data.DefaultTemperature != null) deviceCfg.DefaultTemperature = deviceCfg.data.DefaultTemperature;
+                    if (deviceCfg.MinTemperature == null) if (deviceCfg.data.MinTemperature != null) deviceCfg.MinTemperature = deviceCfg.data.MinTemperature;
+                    if (deviceCfg.MaxTemperature == null) if (deviceCfg.data.MaxTemperature != null) deviceCfg.MaxTemperature = deviceCfg.data.MaxTemperature;
                     LoadedAccessories.push(new MiRemoteAirConditioner(this, deviceCfg));
                     break;
                 case "Custom":
+                    if (deviceCfg.Name == null || deviceCfg.Name == "") deviceCfg.Name = deviceCfg.name || deviceCfg.type;
+                    if (deviceCfg.data == null || deviceCfg.data == "") continue;
                     LoadedAccessories.push(new MiRemoteCustom(this, deviceCfg));
                     break;
                 case "MomentarySwitch":
+                    if (deviceCfg.Name == null || deviceCfg.Name == "") deviceCfg.Name = deviceCfg.name || deviceCfg.type;
+                    if (deviceCfg.data == null || deviceCfg.data == "") continue;
                     LoadedAccessories.push(new MiRemoteMomentarySwitch(this, deviceCfg));
                     break;
-                case "Thermostat":
-                    LoadedAccessories.push(new MiRemoteThermostat(this, deviceCfg));
+                case "LGAirConditioner":
+                    LoadedAccessories.push(new LGAirConditioner(this, deviceCfg));
                     break;
                 default:
-                    this.log.error("device type:", deviceCfg.type, "unsupported.");
+                    this.log.error("Unsupported device type:", deviceCfg.type);
                     break;
             }
 
